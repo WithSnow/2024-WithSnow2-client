e9205943c81dd571ff3ee26fbc2ba1530fa419c7
js-yunn <99jiseon@gmail.com>
2024-08-05 23:28:40 +0900

    feat: 네비게이터 구현 (#2)

--- withsnow/withsnow2/App.js
+++ withsnow/withsnow2/App.js
@@ -1,14 +1,26 @@
 // App.js
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import { SafeAreaView } from 'react-native';
 import styles from './src/styles/map/MapStyles';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import MapScreen from './src/screen/map/MapScreen';
+import MyPageScreen from './src/screen/mypage/MyPageScreen';
 
-const App = () => {
+const Stack = createStackNavigator();
+
+//const App = () => {
+function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <MapScreen />
-    </SafeAreaView>
+    /*<SafeAreaView style={styles.container}>
+      <MyPageScreen />
+    </SafeAreaView>*/
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Map">
+        <Stack.Screen name="Map" component={MapScreen} />
+        <Stack.Screen name="MyPage" component={MyPageScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
---
+++ withsnow/withsnow2/src/component/mypage/MyPageComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import styles from '../../styles/mypage/MyPageStyles';
+
+export default function MyPageComponent() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.mapPlaceholder} />
+        </View>
+    );
+}
