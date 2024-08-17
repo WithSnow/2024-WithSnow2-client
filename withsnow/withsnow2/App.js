a8829bdfd2355f6c1761765b7ad839130b72dee7
js-yunn <99jiseon@gmail.com>
2024-08-18 00:52:41 +0900

    feat: 마이페이지 구현(#7)

--- withsnow/withsnow2/App.js
+++ withsnow/withsnow2/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import 'react-native-reanimated';
 import 'react-native-gesture-handler';
 import MapScreen from './src/screen/map/MapScreen';
@@ -8,7 +8,7 @@ import MyPageScreen from './src/screen/mypage/MyPageScreen';
 import WelfareScreen from './src/screen/welfare/WelfareScreen';
 import FavoriteListScreen from './src/screen/favoriteList/FavoriteListScreen';
 import RecommendPlaceScreen from './src/screen/mypage/recommend/RecommendPlaceScreen';
-import {PlacesProvider} from './src/context/PlacesContext'; // Context 파일 import
+import { PlacesProvider } from './src/context/PlacesContext'; // Context 파일 import
 
 const Stack = createStackNavigator();
 
@@ -21,6 +21,7 @@ function App() {
           <Stack.Screen name="즐겨찾기" component={FavoriteListScreen} />
           <Stack.Screen name="복지사 호출" component={WelfareScreen} />
           <Stack.Screen name="마이페이지" component={MyPageScreen} />
+
 
           {/* 임시 */}
           <Stack.Screen
